f47bf1448fd8ad880dfe8f9c753647b6a6f9eeef
Lutho <luthojama2@gmail.com>
2026-03-15 22:39:28 +0200

    completed project

--- initialData.js
+++ initialData.js
@@ -21,6 +21,8 @@ status:"done"
 }
 ];
 
+// Stores the task that is currently edited
+
 let currentTaskId = null;
 
 function renderTasks(){
@@ -29,17 +31,19 @@ document.getElementById("todo-column").innerHTML="";
 document.getElementById("doing-column").innerHTML="";
 document.getElementById("done-column").innerHTML="";
 
+//Loop through every task in the array
 initialTasks.forEach(task=>{
-
+//Task listner response
 const taskDiv=document.createElement("div");
 
 taskDiv.classList.add("task-div");
 taskDiv.textContent=task.title;
 
+//When a task is clicked open modal
 taskDiv.addEventListener("click",()=>{
 openModal(task.id);
 });
-
+// Place the task in the correct column based on status
 if(task.status==="todo"){
 document.getElementById("todo-column").appendChild(taskDiv);
 }
@@ -56,30 +60,38 @@ document.getElementById("done-column").appendChild(taskDiv);
 
 }
 
+//Open Modal
 function openModal(taskId){
 
+//Find task from array
 const task=initialTasks.find(t=>t.id===taskId);
-
+//Save task ID 
 currentTaskId=taskId;
 
+// Populate modal inputs with current task data
 document.getElementById("modal-title").value=task.title;
 document.getElementById("modal-description").value=task.description;
 document.getElementById("modal-status").value=task.status;
 
+//Show the modal
 document.getElementById("task-modal").classList.remove("hidden");
 
 }
 
+
+//Close modal
 document.getElementById("close-modal").addEventListener("click",()=>{
 
 document.getElementById("task-modal").classList.add("hidden");
 
 });
 
+//Save Changes in modal
 document.getElementById("save-task").addEventListener("click",()=>{
 
 const task=initialTasks.find(t=>t.id===currentTaskId);
 
+//Update modal inputs
 task.title=document.getElementById("modal-title").value;
 task.description=document.getElementById("modal-description").value;
 task.status=document.getElementById("modal-status").value;
