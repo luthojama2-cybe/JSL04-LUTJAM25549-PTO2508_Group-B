ddd549be944026476bfb77b88428a9557346c42a
Lutho <luthojama2@gmail.com>
2026-03-15 19:29:58 +0200

    added corrections to JSL04 fromJSL03

--- initialData.js
+++ initialData.js
@@ -40,3 +40,52 @@ const initialTasks = [
     status: "done",
   },
 ];
+
+// 3 tasks loop
+for (let i = 0; i < 3; i++) {
+
+  let taskTitle = prompt(`Enter title for task ${i + 1}:`);
+  let taskDescription = prompt(`Enter description for task ${i + 1}:`);
+  let taskStatus = prompt(
+    "Enter task status (todo, doing, done):"
+  ).toLowerCase();
+
+  // Status error catching
+  while (
+    taskStatus !== "todo" &&
+    taskStatus !== "doing" &&
+    taskStatus !== "done"
+  ) {
+    alert("Invalid status. Please enter 'todo', 'doing', or 'done'.");
+    taskStatus = prompt(
+      "Enter task status (todo, doing, done):"
+    ).toLowerCase();
+  }
+
+  // Create new ID
+  const newId =
+    Math.max(...initialTasks.map(task => task.id)) + 1;
+
+  const newTask = {
+    id: newId,
+    title: taskTitle,
+    description: taskDescription,
+    status: taskStatus,
+  };
+
+  initialTasks.push(newTask);
+}
+//Chck console message
+alert("There are enough tasks on your board, please check them in the console.");
+
+// Log full list
+console.log("Updated Task List:");
+console.log(initialTasks);
+
+// Completed task log
+const completedTasks = initialTasks.filter(
+  task => task.status === "done"
+);
+
+console.log("Completed Tasks:");
+console.log(completedTasks);
